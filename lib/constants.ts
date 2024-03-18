bf65e3f5201940215c093613a0eb6c04baf6ae8a
Saïkou Barry <bluebeel@users.noreply.github.com>
2024-03-18 14:33:49 +0100

    Update constants.ts

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,4 +1,4 @@
 // Times are in milliseconds
 export const delayShippingEstimate = 400;
-export const delayRecommendedProducts = 800;
+export const delayRecommendedProducts = 10000;
 export const delayReviews = 1000;
